b7bfe2e4bcf15cef30f032d06d7fd10d00ef4a12
sampath-wos <sampathb@whiteorangesoftware.com>
2022-09-19 19:42:18 +0530

    setup tailwind, DarkMode demo, GSAP with timeline and reverse animation demo.

--- portfolio-office/src/App.jsx
+++ portfolio-office/src/App.jsx
@@ -1,36 +1,46 @@
-import { useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import gsap from 'gsap';
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
+import "./App.css";
+import {  } from "react";
+import gsap from "gsap";
 
 function App() {
-  const viteRef = useRef();
-
-  useEffect(() => {
-    // gsap.from(viteRef.current, {x: window.innerWidth/2, xPercent: -50, y: window.innerHeight/2, yPercent: -50, duration: 2, position: "fixed"})
-    gsap.from(viteRef.current, {
-      y: window.innerHeight / 2,
-      yPercent: -50,
-      opacity: 0,
-      scale: 2,
-      duration: 1,
-      ease: "expo.in"
-    })
-
-  },[]);
+  const [popupShow, setPopupshow] = useState(false);
+  const iconsWrapper = useRef();
+  var timelineDemo = gsap.timeline();
+  useLayoutEffect(() => {
+    timelineDemo.from(iconsWrapper.current, { x: -100, duration: 1, delay: 1 });
+  },[])
+  const reverseAnimation = () => {
+    timelineDemo.reverse();
+    console.log("clicked")
+  }
+  const darkMode = () => {
+    setPopupshow(!popupShow)
+  }
 
   return (
-    <div className="App">
-      <div ref={viteRef}>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`App ${popupShow ? "dark" : ""} `}>
+      <div className="home-page dark:bg-black dark:text-white">
+        <div className="icon-wrapper" ref={iconsWrapper}>
+          <a  className="link" href="https://vitejs.dev" target="_blank">
+            <img src={reactLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a className="link" href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+        </div>
+        <div className="hover-details">
+          <h2>Vite</h2>
+          <h2>React</h2>
+        </div>
+        <button onClick={darkMode} >{popupShow ? "LightMode" : "DarkMode"} </button><br/>
+        <button onClick={reverseAnimation}>Reverse</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti nisi facere ducimus repellat temporibus earum. Officia officiis facilis vel. Laborum iste delectus corporis optio vel quia soluta recusandae temporibus?</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
